1df39dfb9b15867b4b042caf1e4447a764905364
ttowncompiled <ian.riley.915@gmail.com>
2019-01-22 22:44:47 -0600

    Adds trial with no collisions.

--- run_exp.py
+++ run_exp.py
@@ -1,25 +1,83 @@
-def run_trial(alpha, beta, n, uavs, victims, adj):
+import random
+
+def run_trial_no_collisions(alpha, beta, n, uavs, victims, adj, turns):
     """
-        Executes a single trial of the experiment with the provided parameters
+        Executes a single trial of the experiment with the provided parameters. Collisions are
+        prohibited.
 
         INPUT:
             alpha - The probability that the UAV's sensors detect a single victim in the same zone,
             beta - The probability that a victim remains in their current zone for the next turn,
             n - The number of zones in the city,
-            uavs - A list of integers representing the zones of the UAVs initial deployment
-                configuration,
-            victims - A list of integers representing the zones of the victims initial deployment
-                configuration, and
-            adj - A dictionary which takes a zone i and returns the zones adjacent to i.
+            uavs - A dictionary of ids to integers representing the zones of the
+                UAVs initial deployment configuration,
+            victims - A dictionary of ids to integers representing the zones of the
+                victims initial deployment configuration,
+            adj - A dictionary which takes a zone i and returns the zones adjacent to i, and
+            turns - The number of turns to run the trial.
         OUTPUT:
-            A tuple containing the two best branches from the game tree - the first excludes
-                collisions while the second includes collisions - both are formatted as a list of
-                tuples where each is a tuple of integers representing the zones of each UAV.
+            A list of the mission-effectiveness of the UAVs after each system player turn.
     """
-
-    return "test"
+    heatmap, results = [ 1.0 / float(n) for _ in range(n) ], []
+    for t in range(turns):
+        # system player turn
+        sys_turn, occupied_zones = {}, set({})
+        for u_id, u_zone in uavs.items():
+            next_zone = u_zone
+            # update location of UAV
+            for a_j in adj[u_zone]:
+                if heatmap[a_j] > heatmap[next_zone] and not a_j in occupied_zones:
+                    next_zone = a_j
+            sys_turn[u_id] = next_zone
+            occupied_zones = occupied_zones.union(set({ next_zone }))
+            # update heatmap after performing a scan
+            heatmap[next_zone] = heatmap[next_zone] * (1 - alpha)
+        uavs = sys_turn
+        # update mission-effectiveness
+        results.append(1 - sum(heatmap))
+        # environmental player turn
+        env_turn = {}
+        for v_id, v_zone in victims.items():
+            thwart = True
+            for u_id, u_zone in uavs.items():
+                if u_zone == v_zone:
+                    # attempt to thwart scan
+                    if random.uniform(0, 1) <= alpha:
+                        thwart = False
+            if not thwart:
+                continue
+            next_zone = v_zone
+            # attempt to move the victim
+            if random.uniform(0, 1) > beta:
+                next_zone = random.choice(adj[v_zone])
+            env_turn[v_id] = next_zone
+        victims = env_turn
+        # update the heatmap to account for the potential moves
+        for i in range(n):
+            heatmap[i] = heatmap[i] * beta
+            for j in adj[i]:
+                heatmap[i] += heatmap[j] * (1 - beta) * 1.0 / len(adj[j])
+    return results
 
 def main():
+    alpha = 0.8
+    beta = 0.8
+    n = 9
+    uavs = { 0: 0, 1: 1, 2: 2 }
+    victims = { 0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8 }
+    adj = {
+        0: [ 1, 3 ],
+        1: [ 0, 2, 4 ],
+        2: [ 1, 5 ],
+        3: [ 0, 4, 6 ],
+        4: [ 1, 3, 5, 7 ],
+        5: [ 2, 4, 8 ],
+        6: [ 3, 7 ],
+        7: [ 6, 4, 8 ],
+        8: [ 7, 5 ]
+    }
+    turns = 10
+    print(run_trial_no_collisions(alpha, beta, n, uavs, victims, adj, turns))
     return "test"
 
 if __name__ == "__main__":
