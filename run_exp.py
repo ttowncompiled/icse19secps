7f6b40a96378b20f894bde7b9d7c342e743a3ffd
ttowncompiled <ian.riley.915@gmail.com>
2019-01-22 23:36:37 -0600

    Completes experiment.

--- run_exp.py
+++ run_exp.py
@@ -1,9 +1,8 @@
 import random
 
-def run_trial_no_collisions(alpha, beta, n, uavs, victims, adj, turns):
+def run_trial(alpha, beta, n, uavs, victims, adj, turns, allow_collisions):
     """
-        Executes a single trial of the experiment with the provided parameters. Collisions are
-        prohibited.
+        Executes a single trial of the experiment with the provided parameters.
 
         INPUT:
             alpha - The probability that the UAV's sensors detect a single victim in the same zone,
@@ -13,8 +12,9 @@ def run_trial_no_collisions(alpha, beta, n, uavs, victims, adj, turns):
                 UAVs initial deployment configuration,
             victims - A dictionary of ids to integers representing the zones of the
                 victims initial deployment configuration,
-            adj - A dictionary which takes a zone i and returns the zones adjacent to i, and
-            turns - The number of turns to run the trial.
+            adj - A dictionary which takes a zone i and returns the zones adjacent to i,
+            turns - The number of turns to run the trial, and
+            allow_collisions - Whether or not collisions are permitted.
         OUTPUT:
             A list of the mission-effectiveness of the UAVs after each system player turn.
     """
@@ -26,7 +26,9 @@ def run_trial_no_collisions(alpha, beta, n, uavs, victims, adj, turns):
             next_zone = u_zone
             # update location of UAV
             for a_j in adj[u_zone]:
-                if heatmap[a_j] > heatmap[next_zone] and not a_j in occupied_zones:
+                if heatmap[a_j] > heatmap[next_zone]:
+                    if a_j in occupied_zones and not allow_collisions:
+                        continue
                     next_zone = a_j
             sys_turn[u_id] = next_zone
             occupied_zones = occupied_zones.union(set({ next_zone }))
@@ -59,24 +61,34 @@ def run_trial_no_collisions(alpha, beta, n, uavs, victims, adj, turns):
                 heatmap[i] += heatmap[j] * (1 - beta) * 1.0 / len(adj[j])
     return results
 
+def run_trials(uavs, victims, n, rows, cols, turns, adj, alphas, betas, trials, allow_collisions):
+    for alpha in alphas:
+        for beta in betas:
+            final_results = [ 0.0 for t in range(turns) ]
+            for trial in range(trials):
+                uav_conf, occupied_zones = {}, set({})
+                for u_id in uavs:
+                    zone = random.randint(0, n-1)
+                    while zone in occupied_zones and not allow_collisions:
+                        zone = random.randint(0, n-1)
+                    uav_conf[u_id] = zone
+                    occupied_zones = occupied_zones.union(set({ zone }))
+                victim_conf = {}
+                for v_id in victims:
+                    victim_conf[v_id] = random.randint(0, n-1)
+                results = run_trial(alpha, beta, n, uav_conf, victim_conf,
+                        adj, turns, allow_collisions)
+                for t in range(turns):
+                    final_results[t] += results[t]
+            for t in range(turns):
+                final_results[t] = final_results[t] / trials
+            print(alpha, beta, final_results[-1])
+
 def main():
-    uavs = [ x for x in range(4) ]
+    uavs = [ x for x in range(20) ]
     victims = [ y for y in range(10) ]
     n, rows, cols = 164, 4, 41
     turns = 60
-    # run trials with no collisions
-    alpha = 0.8
-    beta = 0.8
-    uav_conf, occupied_zones = {}, set({})
-    for u_id in uavs:
-        zone = random.randint(0, n)
-        while zone in occupied_zones:
-            zone = random.randint(0, n)
-        uav_conf[u_id] = zone
-        occupied_zones = occupied_zones.union(set({ zone }))
-    victim_conf = {}
-    for v_id in victims:
-        victim_conf[v_id] = random.randint(0, n)
     adj = {}
     for i in range(n):
         adj_list = []
@@ -89,8 +101,11 @@ def main():
         if i % 41 != 0:
             adj_list.append( i - 1 )
         adj[i] = adj_list
-    print(run_trial_no_collisions(alpha, beta, n, uav_conf, victim_conf, adj, turns))
-    return "test"
+    alphas = [ 0.2, 0.4, 0.6, 0.8, 1.0 ]
+    betas = [ 0.2, 0.4, 0.6, 0.8, 1.0 ]
+    trials = 20
+    run_trials(uavs, victims, n, rows, cols, turns, adj, alphas, betas, trials, False)
+    run_trials(uavs, victims, n, rows, cols, turns, adj, alphas, betas, trials, True)
 
 if __name__ == "__main__":
     main()
