1f3296b89acaea3309f5dfd3676336105514da82
ttowncompiled <ian.riley.915@gmail.com>
2019-02-05 15:23:44 -0600

    Updates experiment to include new posed requirement. Results are included.

--- run_exp.py
+++ run_exp.py
@@ -4,126 +4,148 @@ import pandas as pd
 import random
 import seaborn as sns
 
-def run_trial(alpha, beta, n, uavs, victims, adj, turns, allow_collisions):
+def run_trial(alpha, beta, n, uav_conf, victim_conf, adj, turns, collisions=False, stay_put=False):
     """
         Executes a single trial of the experiment with the provided parameters.
 
         INPUT:
-            alpha - The probability that the UAV's sensors detect a single victim in the same zone,
-            beta - The probability that a victim remains in their current zone for the next turn,
+            alpha - The probability that a victim remains in their current zone for the next turn,
+            beta - The probability that the UAV's sensors detect a victim in the same zone,
             n - The number of zones in the city,
-            uavs - A dictionary of ids to integers representing the zones of the
-                UAVs initial deployment configuration,
-            victims - A dictionary of ids to integers representing the zones of the
-                victims initial deployment configuration,
+            uav_conf - A list of indices representing the locations of the UAVs,
+            victim_conf - A list of indices representing the locations of the victims,
             adj - A dictionary which takes a zone i and returns the zones adjacent to i,
-            turns - The number of turns to run the trial, and
-            allow_collisions - Whether or not collisions are permitted.
+            turns - The number of turns to run the trial,
+            collisions - Whether collisions should be minimized or ignored, and
+            stay_put - Whether half the UAVs should remain at their initial location or all move.
         OUTPUT:
-            A list of the 4-tuples (alpha, beta, mission-effectiveness, num victims not found).
+            A list of triples (alpha, beta, accrued-reward).
     """
-    heatmap, eff = [ 1.0 / float(n) for _ in range(n) ], 0.0
+    heatmap, reward = [ 1.0 / float(n) for _ in range(n) ], 0.0
     for t in range(turns):
         # system player turn
         sys_turn = []
-        for u_zone in uavs:
+        idx = 0
+        for u_zone in uav_conf:
             next_zone = u_zone
             # update location of UAV
-            for a_j in adj[u_zone]:
-                if heatmap[a_j] > heatmap[next_zone]:
-                    if a_j in sys_turn and not allow_collisions:
-                        continue
-                    next_zone = a_j
+            if not stay_put or idx % 2 == 0:
+                for a_j in adj[u_zone]:
+                    if heatmap[a_j] > heatmap[next_zone]:
+                        if a_j in sys_turn and collisions:
+                            continue
+                        next_zone = a_j
             sys_turn.append(next_zone)
-        uavs = sys_turn
-        for next_zone in uavs:
+            idx += 1
+        uav_conf = sys_turn
+        for next_zone in uav_conf:
             # update heatmap after performing a scan
-            heatmap[next_zone] = heatmap[next_zone] * (1 - alpha)
-        # update mission effectiveness
-        eff = 1 - sum(heatmap)
+            heatmap[next_zone] = heatmap[next_zone] * (1 - beta)
+        # update accrued reward
+        reward = 1 - sum(heatmap)
         # environmental player turn
         env_turn = []
-        for v_zone in victims:
+        for v_zone in victim_conf:
             thwart = True
-            for u_zone in uavs:
+            for u_zone in uav_conf:
                 if u_zone == v_zone:
                     # attempt to thwart scan
-                    if random.uniform(0, 1) <= alpha:
+                    if random.uniform(0, 1) <= beta:
                         thwart = False
             if not thwart:
                 continue
             next_zone = v_zone
             # attempt to move the victim
-            if random.uniform(0, 1) > beta:
+            if random.uniform(0, 1) > alpha:
                 next_zone = random.choice(adj[v_zone])
             env_turn.append(next_zone)
-        victims = env_turn
+        victim_conf = env_turn
         # update the heatmap to account for the potential moves
         for i in range(n):
-            heatmap[i] = heatmap[i] * beta
+            heatmap[i] = heatmap[i] * alpha
             for j in adj[i]:
-                heatmap[i] += heatmap[j] * (1 - beta) * 1.0 / len(adj[j])
-    return (alpha, beta, eff, len(victims))
+                heatmap[i] += heatmap[j] * (1 - alpha) * 1.0 / len(adj[j])
+    return (alpha, beta, reward)
 
 
-def run_trials(uavs, victims, n, rows, cols, turns, adj, alphas, betas, trials, seed):
-    random.seed(seed)
-    r1, r2 = [], []
+def run_trials(uav_count, victim_count, n, rows, cols, adj, alphas, betas, t, state):
+    r1, r2, r3 = [], [], []
     for alpha in alphas:
         for beta in betas:
-            for trial in range(trials):
-                uav_conf = []
-                for u_i in range(uavs):
-                    uav_conf.append(random.randint(0, n-1))
-                victim_conf = []
-                for v_j in range(victims):
-                    victim_conf.append(random.randint(0, n-1))
-                trial_results = run_trial(alpha, beta, n, uav_conf, victim_conf,
-                        adj, turns, True)
-                r1.append(trial_results)
-                trial_results = run_trial(alpha, beta, n, uav_conf, victim_conf,
-                        adj, turns, False)
-                r2.append(trial_results)
-    return r1, r2
+            random.setstate(state)
+            uav_conf = []
+            for u_i in range(uav_count):
+                uav_conf.append(random.randint(0, n-1))
+            victim_conf = []
+            for v_j in range(victim_count):
+                victim_conf.append(random.randint(0, n-1))
+            trial_results = run_trial(alpha, beta, n, uav_conf, victim_conf,
+                    adj, t, collisions=False, stay_put=False)
+            r1.append(trial_results)
+            trial_results = run_trial(alpha, beta, n, uav_conf, victim_conf,
+                    adj, t, collisions=True, stay_put=False)
+            r2.append(trial_results)
+            trial_results = run_trial(alpha, beta, n, uav_conf, victim_conf,
+                    adj, t, collisions=False, stay_put=True)
+            r3.append(trial_results)
+    return r1, r2, r3
 
 
-def graph_results(r1, r2, title, diff_only=False, set_lims=True):
-    if not diff_only:
-        data = []
-        for p_i in r1:
-            data.append([p_i[0], p_i[1], p_i[2]])
-        df = pd.DataFrame(data, columns=['X', 'Y', 'Z'])
-        fig = plt.figure()
-        ax = fig.gca(projection='3d')
-        ax.plot_trisurf(df['X'], df['Y'], df['Z'], cmap=plt.cm.jet, linewidth=0.01)
-        ax.set_title('Best Accrued Reward across values of Alpha and Beta')
-        ax.set_xlabel('beta (β)')
-        ax.set_ylabel('alpha (α)')
-        ax.set_zlabel('accrued reward (R)')
-        plt.show()
-        plt.savefig(title + '.png')
+def graph_results(r1, r2, r3, title):
+    data = []
+    for p_i in r1:
+        data.append([p_i[0], p_i[1], p_i[2]])
+    df = pd.DataFrame(data, columns=['X', 'Y', 'Z'])
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ax.plot_trisurf(df['Y'], df['X'], df['Z'], cmap=plt.cm.jet, linewidth=0.01)
+    ax.set_title('Best Accrued Reward across values of Alpha and Beta')
+    ax.set_ylabel('alpha (α)')
+    ax.set_xlabel('beta (β)')
+    ax.set_zlabel('accrued reward (R)')
+    plt.show()
+    plt.savefig(title + '.png')
     delta = []
     for i in range(len(r1)):
         t1, t2 = r1[i], r2[i]
-        delta.append((t1[0], t1[1], t2[2]-t1[2], t2[3] - t1[3]))
+        delta.append((t1[0], t1[1], t2[2]-t1[2]))
     data = []
     for d_i in delta:
         data.append([d_i[0], d_i[1], d_i[2]])
     df = pd.DataFrame(data, columns=['X', 'Y', 'Z'])
     fig = plt.figure()
     ax = fig.gca(projection='3d')
-    ax.plot_trisurf(df['X'], df['Y'], df['Z'], cmap=plt.cm.jet, linewidth=0.01)
+    ax.plot_trisurf(df['Y'], df['X'], df['Z'], cmap=plt.cm.jet, linewidth=0.01)
     ax.set_title('Differential Accrued Reward across values of Alpha and Beta')
-    ax.set_xlabel('beta (β)')
     ax.set_ylabel('alpha (α)')
+    ax.set_xlabel('beta (β)')
     ax.set_zlabel('accrued reward (R)')
     plt.show()
-    plt.savefig(title + '.diff.png')
+    plt.savefig(title + '.collisions.diff.png')
+    delta = []
+    for i in range(len(r1)):
+        t1, t2 = r1[i], r3[i]
+        delta.append((t1[0], t1[1], t2[2]-t1[2]))
+    data = []
+    for d_i in delta:
+        data.append([d_i[0], d_i[1], d_i[2]])
+    df = pd.DataFrame(data, columns=['X', 'Y', 'Z'])
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ax.plot_trisurf(df['Y'], df['X'], df['Z'], cmap=plt.cm.jet, linewidth=0.01)
+    ax.set_title('Differential Accrued Reward across values of Alpha and Beta')
+    ax.set_ylabel('alpha (α)')
+    ax.set_xlabel('beta (β)')
+    ax.set_zlabel('accrued reward (R)')
+    plt.show()
+    plt.savefig(title + '.stay.diff.png')
     plt.close('all')
 
 
 def main():
     seed = 695480078
+    random.seed(seed)
+    state = random.getstate()
     uavs = [4, 8, 16, 24, 32, 64, 128]
     victims = 10
     n, rows, cols = 164, 4, 41
@@ -142,14 +164,10 @@ def main():
         adj[i] = adj_list
     alphas = [ float(x+1) / 20.0 for x in range(20) ]
     betas = [ float(x+1) / 20.0 for x in range(20) ]
-    trials = 1
     for t in turns:
-        u1, u2 = run_trials(uavs[0], victims, n, rows, cols, t, adj, alphas, betas, trials, seed)
-        graph_results(u1, u2, 'results-' + str(t) + '/fig.' + str(uavs[0]))
-        for j in range(1, len(uavs)):
-            r1, r2 = run_trials(uavs[j], victims, n, rows, cols, t, adj, alphas, betas, trials, seed)
-            graph_results(r1, r2, 'results-' + str(t) + '/fig.' + str(uavs[j]))
-            graph_results(u1, r1, 'results-' + str(t) + '/fig.4.' + str(uavs[j]), diff_only=True, set_lims=False)
+        for uav in uavs:
+            r1, r2, r3 = run_trials(uav, victims, n, rows, cols, adj, alphas, betas, t, state)
+            graph_results(r1, r2, r3, 'results-' + str(t) + '/fig.' + str(uav))
 
 
 if __name__ == "__main__":
